3f488952b3ee7aca3d22ed1453604f2bfaa69b7d
Andrey Loskutov <joofer.sp@gmail.com>
2022-11-11 13:59:06 +0300

    added email unsibscribe page

---
+++ Pages/Unsubscribe.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using razor_secret_santa.Data;
+using razor_secret_santa.Models;
+
+namespace razor_secret_santa.Pages
+{
+    [BindProperties(SupportsGet = true)]
+    public class UnsubscribeModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public int userID { get; set; }
+
+        public UnsubscribeModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            var userModel = _context.UserModels.SingleOrDefault(u => u.id == userID);
+            userModel!.email = null;
+            _context.SaveChanges();
+            return Page();
+        }
+    }
+}
